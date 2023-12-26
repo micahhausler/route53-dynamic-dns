6ef61f32ce08fc2bcca84ce6c61f277da8bce95c
Micah Hausler <hausler.m@gmail.com>
2023-12-26 14:09:00 -0600

    Fixed shasum check

--- route53-dynamic-dns-systemd.sh
+++ route53-dynamic-dns-systemd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 # Load environment variables
 set -a
@@ -26,23 +27,24 @@ create_services() {
 install_binary() {
 	if [ ! -f "${ROUTE53_DYNAMIC_DNS_BINARY_PATH}" ]; then
 		echo "install_binary(): Downloading route53-dynamic-dns binary"
-		wget -qO "/tmp/r53_d_d_release-${ROUTE53_DYNAMIC_DNS_VERSION}.tar.gz" "${ROUTE53_DYNAMIC_DNS_DOWNLOAD_URL}"
-		chmod +x "${ROUTE53_DYNAMIC_DNS_BINARY_PATH}"
-
-		echo "install_binary(): Extracting route53-dynamic-dns binary from release and placing at ${ROUTE53_DYNAMIC_DNS_BINARY_PATH}"
-		tar -xozvf "/tmp/r53_d_d_release-${ROUTE53_DYNAMIC_DNS_VERSION}.tar.gz" --directory="${ROUTE53_DYNAMIC_DNS_PATH}" route53-dynamic-dns
+        tmp_tarball="/tmp/r53_d_d_release-${ROUTE53_DYNAMIC_DNS_VERSION}.tar.gz"
+		wget -qO "${tmp_tarball}" "${ROUTE53_DYNAMIC_DNS_DOWNLOAD_URL}"
 
 		# Verify lego binary integrity
-		echo "install_binary(): Verifying integrity of route53-dynamic-dns binary"
-		R53DD_HASH=$(sha1sum "${ROUTE53_DYNAMIC_DNS_BINARY}" | awk '{print $1}')
+		echo "install_binary(): Verifying integrity of route53-dynamic-dns tarball"
+		R53DD_HASH=$(sha1sum "${tmp_tarball}" | awk '{print $1}')
 		if [ "${R53DD_HASH}" = "${ROUTE53_DYNAMIC_DNS_SHA1}" ]; then
 			echo "install_binary(): Verified route53-dynamic-dns v${ROUTE53_DYNAMIC_DNS_VERSION}:${R53DD_HASH}"
+
+            echo "install_binary(): Extracting route53-dynamic-dns binary from release and placing at ${ROUTE53_DYNAMIC_DNS_BINARY_PATH}"
+            tar -xozvf ${tmp_tarball} --directory="${ROUTE53_DYNAMIC_DNS_PATH}" route53-dynamic-dns
 			chmod +x "${ROUTE53_DYNAMIC_DNS_BINARY}"
 		else
-			echo "install_binary(): Verification failure, route53-dynamic-dns binary sha1 was ${R53DD_HASH}, expected ${ROUTE53_DYNAMIC_DNS_SHA1}. Cleaning up and aborting"
-			rm -f "${ROUTE53_DYNAMIC_DNS_BINARY_PATH}" "/tmp/r53_d_d_release-${ROUTE53_DYNAMIC_DNS_VERSION}.tar.gz"
+			echo "install_binary(): Verification failure, route53-dynamic-dns tarball sha1 was ${R53DD_HASH}, expected ${ROUTE53_DYNAMIC_DNS_SHA1}. Cleaning up and aborting"
+			rm -f "${tmp_tarball}"
 			exit 1
 		fi
+
 	else
 		echo "install_binary(): route53-dynamic-dns binary is already installed at ${ROUTE53_DYNAMIC_DNS_BINARY_PATH}, no operation necessary"
 	fi
